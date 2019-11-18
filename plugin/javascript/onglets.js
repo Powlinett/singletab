5e03e64c6944660c1ed367f29565119f6a0bddd1
jonathan chaffanjon <platonformation@gmail.com>
2019-11-18 18:42:39 +0100

    liste avec url et sur une seul fenetre chrome

--- plugin/javascript/onglets.js
+++ plugin/javascript/onglets.js
@@ -1,20 +1,18 @@
 const liste = document.getElementById('listeonglets');
-    console.log(liste)
-    liste.insertAdjacentHTML("afterend", "<li>Anakin</li>");
 
-  chrome.windows.getAll({populate:true},function(windows){
-    windows.forEach(function(onglet){
-      onglet.tabs.forEach(function(x){
+   chrome.tabs.query({currentWindow: true}, function(tabs) {
+      tabs.forEach(function(tab) {
+        console.log(liste)
+
        //collect all of the urls here, I will just log them instead
       liste.insertAdjacentHTML("afterend",
           `<div class="card m-3">
             <div class="card-header">
-              ${x.title} <img src="${x.favIconUrl}" alt="">
+              ${tab.title} <img src="${tab.favIconUrl}" alt="">
             </div>
             <div class="card-body">
-              <a href="${x.url}" class="btn btn-primary">Open</a>
+              <a href="${tab.url}" class="btn btn-primary">Open</a>
             </div>
           </div>`);
       });
     });
-  });
