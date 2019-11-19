48837c89184ea6dd0ce6a18c806de2ae11f3fe0c
jonathan chaffanjon <platonformation@gmail.com>
2019-11-19 11:24:57 +0100

    tabs_controller methode create

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,2 +1,31 @@
 class TabsController < ApplicationController
+
+
+  def index
+    @tabs = Tab.all
+  end
+
+  def new
+    @project = Tab.new
+  end
+ def create
+  @onglet = Onglet.new(
+    name: `Search #{Times.new}`,
+    weight: 1,
+    user_id: current_user
+    )
+  if @onglet.save
+    @tab = Tab.new(
+    name: tab.title,
+    url: tab.url,
+    title: tab.title,
+    icon: tab.favIconUrl,
+    description: "",
+    comment: "",
+    folder_id: @onglet.id
+    )
+  else
+    format.html { render :index }
+  end
+  end
 end
