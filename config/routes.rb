6ff7de5a882a57ba7e8dbd017fd67e81b064bf0b
eric-wlw <weloveworks@gmail.com>
2019-11-19 19:00:11 +0100

    d3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/visualisation', to: 'pages#visualisation'
+  get '/data', to: 'pages#data'
+
   resources :folders, only: [:index, :show]
   resources :tabs, only: [:edit, :create]
 
