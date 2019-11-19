fb8eddcd3d469c68084cd9c10b0d11cc0b3dfa03
Teix <marcoteixeira.57@gmail.com>
2019-11-19 10:13:47 +0100

    update manifest
    update manifest

--- plugin/javascript/background.js
+++ plugin/javascript/background.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => { //pour attendre creation de pages
  var button = document.getElementById('checkPage');
  button.addEventListener('click', (event) => {
-    window.open('onglet.html','_blank') //ouvre view onglets#index
+    window.open('onglets.html','_blank') //ouvre view onglets#index
   // window.location.replace('onglets.html')
      });
    });
