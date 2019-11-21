2d21dc94457d25ed653b94e52f2106cee12d12ef
jonathan chaffanjon <platonformation@gmail.com>
2019-11-21 13:11:03 +0100

    java sur data avec multi folder et tabs

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -20,19 +20,19 @@ class TabsController < ApplicationController
       arrayparams = JSON.parse(params[:variable])
       arrayparams.each do |tab|
 
-      url = Domainatrix.parse(tab["url"])
+        url = Domainatrix.parse(tab["url"])
 
-      @tab = Tab.new(
-      name: url.domain,
-      url: tab["url"],
-      title: no_accent(tab["title"]),
-      icon: tab["icon"],
-      description: tab["body"],
-      comment: "",
+        @tab = Tab.new(
+          name: url.domain,
+          url: tab["url"],
+          title: no_accent(tab["title"]),
+          icon: tab["icon"],
+          description: tab["body"],
+          comment: "",
       folder_id: @folder.id #voir ajout couleur
       )
-      @tab.save!
-    end
+        @tab.save!
+      end
       redirect_to folders_path
     end
   end
@@ -45,7 +45,7 @@ class TabsController < ApplicationController
     redirect_to folders_path
   end
 
-private
+  private
   def no_accent(texte)
     CGI.escape(texte).gsub('+',' ').gsub('%E9','e').gsub('%27',' ').gsub('%FB','u').gsub('%2B','+').gsub('%28','(').gsub('%3B',';').gsub('%0D%0A','').gsub('%2C',',').gsub('%85','...').gsub('%29',')').gsub('%EA','e').gsub('%26','et').gsub('%3A',':').gsub('%E2','a').gsub('%E0','a').gsub('%22','').gsub('%8C','OE').gsub('%B0','um ').gsub('%E8','e')
   end
