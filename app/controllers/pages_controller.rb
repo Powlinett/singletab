852adfe3a6117ef80dd6fd1acbadd56cfc576ed0
jonathan chaffanjon <platonformation@gmail.com>
2019-11-25 17:32:35 +0100

    multi branch data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,38 +10,8 @@ class PagesController < ApplicationController
     condition = params[:id].nil? ? " " : " AND folders.id = #{params[:id]}"
     @folders = Folder.where("folders.user_id = #{current_user.id} #{condition}")
 
-
     render json: mind_map()
 
-    folders.each do |f|
-      arrayfolder = {
-        "name": "#{f.name}",
-        "id": "#{f.id}",
-        "children": ""
-      }
-      f.tabs.all.each do |t|
-        tabsfolder << {
-          "name": t.name,
-          "id": t.id,
-          "size": 1,
-          "url": t.url,
-          "title": t.title,
-          "favicon": t.icon
-        }
-      end
-      arrayfolder[:children] = tabsfolder
-      tabsfolder = []
-      allfolder << arrayfolder
-    end
-
-    render json: {
-      "name": "My map",
-      "children": [{
-        "name": "My search",
-        "size": 3,
-        "children": allfolder
-      }]
-    }
   end
 
   def auth
@@ -64,45 +34,45 @@ class PagesController < ApplicationController
   end
 
   # /test code enzo by jojo
-   def superparent()
-      childrens = []
-      @folders.each do |item|
-        if item.parent_id.nil?
-          hash = { name: item.name, id: item.id }
-          hash[:size] = 3
-          hash[:children] = show_child(item.id)
-          childrens << hash
+ def superparent()
+    childrens = []
+    @folders.each do |item|
+      if item.parent_id.nil?
+        hash = { name: item.name, id: item.id, parent_id: item.parent_id}
+        hash[:children] = alltabs(item)
+        if item.folders
+          item.folders.each do |item|
+            hash[:children] << add_child(item)
+          end
         end
+        childrens << hash
       end
-        childrens
+    end
+      childrens
+  end
+
+   def add_child(item)
+        hash = { name: item.name, id: item.id }
+        hash[:children] = alltabs(item)
+        if item.folders
+          item.folders.each do |item|
+            hash[:children] << add_child(item)
+          end
+        end
+        hash
     end
 
-   def show_child(folder_id)
+    def alltabs(folder)
       childrens = []
-      @folders.each do |item|
-        if item.parent_id = folder_id
-          hash = { name: item.name, id: item.id }
-          hash[:size] = 3
-          hash[:children] = alltabs(item.id)
-          childrens << hash
-        end
-      end
-        childrens
-    end
-
-    def alltabs(folder_id)
-      childrens = []
-      @folders.each do |item|
-        if item..tabs.all.each do |t|
-          tabsfolder << {
-            "name": t.name,
-            "id": t.id,
-            "size": 1,
-            "url": t.url,
-            "title": t.title,
-            "favicon": t.icon
-          }
-        end
+      folder.tabs.all.each do |t|
+        childrens << {
+          name: t.name,
+          id: t.id,
+          size: 1,
+          url: t.url,
+          title: t.title,
+          favicon: t.icon
+        }
       end
         childrens
     end
