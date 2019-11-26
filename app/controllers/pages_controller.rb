074125b30dc04e2d46f257537e524645f5ccbe61
eric-wlw <weloveworks@gmail.com>
2019-11-26 17:14:49 +0100

    plugin et favicon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -68,7 +68,7 @@ class PagesController < ApplicationController
     def alltabs(folder)
       childrens = []
       folder.tabs.all.each do |t|
-        t.icon = t.icon.nil? ? "https://files.slack.com/files-pri/T02NE0241-FQYSWCS20/singletab_black_.png" : t.icon
+        t.icon = t.icon.nil? ? "https://files.slack.com/files-pri/T02NE0241-FQRJUMMU1/singletab_white_black.png?pub_secret=9619954ba4" : t.icon
         childrens << {
           name: t.name,
           id: t.id,
@@ -87,9 +87,16 @@ class PagesController < ApplicationController
       arrayfolder = Folder.search_folder_by_id(current_user.id)
       map = {
         name: "My map",
-        favicon: "https://files.slack.com/files-pri/T02NE0241-FQYSWCS20/singletab_black_.png",
+        favicon: "https://files.slack.com/files-pri/T02NE0241-FQRJUMMU1/singletab_white_black.png?pub_secret=9619954ba4",
         children: superparent()
       }
       map.to_json
     end
 end
+
+
+
+
+
+
+
