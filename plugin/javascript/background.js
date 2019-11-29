bf6f43a71cb19469e0405c8c9a453550336c9c57
Pauline <pauline.eustachy@gmail.com>
2019-11-29 12:29:23 +0100

    url prod

--- plugin/javascript/background.js
+++ plugin/javascript/background.js
@@ -1,9 +1,8 @@
+const urlsite =  'https://still-lowlands-24985.herokuapp.com/checkauth'
+const urlsitetabs =  'https://still-lowlands-24985.herokuapp.com/tabs'
 
-//const urlsite =  'https://still-lowlands-24985.herokuapp.com/checkauth'
-//const urlsitetabs =  'https://still-lowlands-24985.herokuapp.com/tabs'
-
-const urlsite = 'http://localhost:3000/checkauth'
-const urlsitetabs = 'http://localhost:3000/tabs'
+// const urlsite = 'http://localhost:3000/checkauth'
+// const urlsitetabs = 'http://localhost:3000/tabs'
 
 fetch(urlsite)
 .then((response) => { return (response.json()) })
